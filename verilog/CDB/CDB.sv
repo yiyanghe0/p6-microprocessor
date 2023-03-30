ddb153837117ea0976d86cfa1b4eede9fcfa3c1f
boblbwee <boblbwee@umich.edu>
2023-03-30 00:20:49 -0400

    fixed cdb

--- verilog/CDB/CDB.sv
+++ verilog/CDB/CDB.sv
@@ -10,7 +10,7 @@ module CDB(
     assign cdb_packet_out.reg_tag.tag 	= ex_packet_in.dest_reg_idx;
     assign cdb_packet_out.reg_tag.valid = (ex_packet_in.is_ZEROREG) ? 0 : 1;
     assign cdb_packet_out.reg_value     = ex_packet_in.alu_result;
-    assign cdb_packet_out.NPC 		    = ex_packet_in.NPC;
+    assign cdb_packet_out.NPC 		    = (ex_packet_in.take_branch) ? ex_packet_in.alu_result : ex_packet_in.NPC;
     assign cdb_packet_out.take_branch   = ex_packet_in.take_branch;
 endmodule
 
